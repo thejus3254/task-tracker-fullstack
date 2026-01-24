f848d3362ec8faeea91d306ed1a7c74e9b04c2ca
thejusaihu <thejusaishu@gmail.com>
2026-01-24 17:40:30 +0530

    feat: add echo and greet endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express");  //main express framework
+const cors = require("cors"); //middleware to enable CORS
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express();  //main express app
+app.use(cors());    //middleware to enable CORS
+app.use(express.json());  //middleware to parse JSON bodies
 
-app.get("/health", (req, res) => {
+app.get("/health", (req, res) => {    //health check endpoint
   res.json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`API running on ${PORT}`));
+app.get("/greet",(req,res)=> {
+    res.json({message:"Hello Thejus!!"});
+});
+
+app.post("/echo", (req,res)=>{  //echo endpoint to return the received data
+    res.json({
+        message: "I received your data",
+        youSent: req.body});
+})
+
+const PORT = process.env.PORT || 4000;     //set port to environment variable or default to 4000
+app.listen(PORT, () => console.log(`API running on ${PORT}`));  //start server
